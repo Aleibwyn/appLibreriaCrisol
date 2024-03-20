14196dc20cefc9835d17672744a338ce2489a444
ewkwerd <ewkwerd@hotmail.com>
2024-03-20 01:16:51 -0500

    feat(login): Diseño y validación básica del inicio de sesión

---
+++ app/src/main/java/pe/edu/crisol/libreria/login/LoginActivity.kt
@@ -0,0 +1,42 @@
+package pe.edu.crisol.libreria.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.widget.Toast
+import pe.edu.crisol.libreria.R
+import pe.edu.crisol.libreria.databinding.ActivityLoginBinding
+import pe.edu.crisol.libreria.menu.MenuActivity
+
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btRegister.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+
+        binding.btLogin.setOnClickListener {
+            val email = binding.htEmail.text.toString()
+            val password = binding.htPassword.text.toString()
+            isValidCredentials(email, password)
+        }
+    }
+
+    private fun isValidCredentials(email: String, password: String) {
+        val validEmail = "Rox"
+        val validPassword = "Nga2814"
+
+        if (email == validEmail && password == validPassword) {
+            startActivity(Intent(this, MenuActivity::class.java))
+        } else {
+            Toast.makeText(this, "Credenciales incorrectas. Por favor, inténtalo de nuevo.", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
